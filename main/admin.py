75cc7154bf07f23c31df22f4b6898f5d267436ca
Anton Vedischev <vedishchevanton@gmail.com>
2023-06-16 15:44:33 +0300

    реализация админки

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
+from main.models import Customer, Newsletter, Message, Log
+
+
 # Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'comment')
+    list_filter = ('full_name', 'email', 'comment')
+    search_fields = ('full_name', 'email')
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('send_time', 'frequency', 'status')
+    list_filter = ('frequency', 'status')
+    search_fields = ('customers__first_name', 'customers__last_name', 'customers__email')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'body')
+    list_filter = ('subject', 'body')
+    search_fields = ('subject', 'body')
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('message', 'timestamp', 'status')
+    list_filter = ('message', 'timestamp', 'status')
+    search_fields = ('message', 'timestamp', 'status')
--- main/models.py
+++ main/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Customer(models.Model):
-    email = models.EmailField()  # контактный email клиента
-    full_name = models.CharField(max_length=100)  # ФИО клиента
-    comment = models.TextField()  # комментарий клиента
+    email = models.EmailField(verbose_name='Контактный email')
+    full_name = models.CharField(max_length=100, verbose_name='ФИО')
+    comment = models.TextField(verbose_name='Комментарий')
 
     def __str__(self):
         """Возвращает строковое представление модели."""
@@ -25,11 +25,12 @@ class Newsletter(models.Model):
         ('monthly', 'Ежемесячно'),
     )
     send_time = models.TimeField()  # время рассылки
-    frequency = models.CharField(max_length=10,
+    frequency = models.CharField(max_length=10, verbose_name='Периодичность рассылки',
                                  choices=SEND_FREQUENCY_CHOICES)  # периодичность рассылки (раз в день, раз в неделю,
     # раз в месяц)
-    status = models.CharField(max_length=10)  # статус рассылки (завершена, создана, запущена)
-    customers = models.ManyToManyField('Customer',
+    status = models.CharField(max_length=10,
+                              verbose_name='Статус рассылки')  # статус рассылки (завершена, создана, запущена)
+    customers = models.ManyToManyField('Customer', verbose_name='Клиенты',
                                        related_name='newsletters')  # связь многие-ко-многим с моделью Клиенты
 
     def __str__(self):
@@ -44,9 +45,10 @@ class Newsletter(models.Model):
 
 class Message(models.Model):
     """Модель Сообщение для рассылки"""
-    newsletter = models.ForeignKey(Newsletter, on_delete=models.CASCADE)  # внешний ключ на модель Рассылка
-    subject = models.CharField(max_length=200)  # тема письма
-    body = models.TextField()  # тело письма
+    newsletter = models.ForeignKey(Newsletter, verbose_name='Рассылка',
+                                   on_delete=models.CASCADE)  # внешний ключ на модель Рассылка
+    subject = models.CharField(max_length=200, verbose_name='Тема письма')  # тема письма
+    body = models.TextField(verbose_name='Содержание письма')  # тело письма
 
     def __str__(self):
         """Возвращает строковое представление модели."""
@@ -54,15 +56,20 @@ class Message(models.Model):
 
     class Meta:
         """Метаданные модели."""
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
         ordering = ['-id']  # сортировка записей в базе данных по убыванию id
 
 
 class Log(models.Model):
     """Модель Логи рассылки"""
-    message = models.ForeignKey(Message, on_delete=models.CASCADE)  # внешний ключ на модель Сообщение для рассылки
-    timestamp = models.DateTimeField(auto_now_add=True)  # дата и время последней попытки
-    status = models.CharField(max_length=20)  # статус попытки
-    response = models.TextField()  # ответ почтового сервера, если он был
+    message = models.ForeignKey(Message, on_delete=models.CASCADE,
+                                verbose_name='Сообщение для рассылки')  # внешний ключ на модель Сообщение для рассылки
+    timestamp = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата и время последней попытки')  # дата и время последней попытки
+    status = models.CharField(max_length=20, verbose_name='Статус попытки')  # статус попытки
+    response = models.TextField(
+        verbose_name='Ответ почтового сервера, если он был')  # ответ почтового сервера, если он был
 
     def __str__(self):
         """Возвращает строковое представление модели."""
