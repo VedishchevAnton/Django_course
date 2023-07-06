ffbd91d75498a6a0ba5f588df161729e5a1c7e38
Anton Vedischev <vedishchevanton@gmail.com>
2023-07-06 19:01:18 +0300

    реализация отправки сообщения пользователю

--- main/services.py
+++ main/services.py
@@ -1,6 +1,19 @@
+from django.core.mail import send_mail
+
+from Django_course import settings
 from main.models import Message
 
 
-def send_newsletter(message_id):
-    subject = Message.objects.get(pk=message_id)
-pass
+def send_newsletter(message_item: Message):
+    # Получаем список email-адресов клиентов, которым нужно отправить рассылку
+    customers_emails = message_item.newsletter.customers.values_list('email', flat=True)
+
+    # Отправляем письмо каждому клиенту
+    for email in customers_emails:
+        send_mail(
+            message_item.subject,  # Тема письма
+            message_item.body,  # Тело письма
+            settings.EMAIL_HOST_USER,  # От кого отправляем письмо
+            [email],  # Кому отправляем письмо
+            fail_silently=False,
+        )
--- main/views_package/message_views/message_create_view.py
+++ main/views_package/message_views/message_create_view.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from main.models import Message
+from main.services import send_newsletter
 
 
 class MessageCreateView(generic.CreateView):
@@ -8,3 +9,8 @@ class MessageCreateView(generic.CreateView):
     template_name = 'main/messages/message_form.html'
     fields = ['newsletter', 'subject', 'body']
     success_url = reverse_lazy('main:messages')
+
+    def form_valid(self, form):
+        obj = form.save()
+        send_newsletter(obj)
+        return super().form_valid(form)
